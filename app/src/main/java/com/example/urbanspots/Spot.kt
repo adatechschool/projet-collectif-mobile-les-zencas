d438a147947794753751ec92773519c0daaea043
zachary <z.joriot@gmail.com>
2024-05-07 11:03:10 +0200

    black background

--- app/src/main/java/com/example/urbanspots/Spot.kt
+++ app/src/main/java/com/example/urbanspots/Spot.kt
@@ -2,18 +2,21 @@ package com.example.urbanspots
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SpotScreen() {
+
     val images = listOf(
         R.drawable.space1,
         R.drawable.space2,
@@ -21,7 +24,7 @@ fun SpotScreen() {
     )
     val pagerState = rememberPagerState(pageCount = { images.size })
 
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize().background(Color.Black)) {
         HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { page ->
             Image(
                 painter = painterResource(id = images[page]),
