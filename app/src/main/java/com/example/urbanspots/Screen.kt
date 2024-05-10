f885b5bba5e180f952715936692741351d7032c0
Apolline Diaz <apollinediaz@apollines-mbp.home>
2024-05-10 23:51:10 +0200

    Navigation between SpotList aka MainScreen and DetailScreen

---
+++ app/src/main/java/com/example/urbanspots/Screen.kt
@@ -0,0 +1,17 @@
+package com.example.urbanspots
+
+sealed class Screen (val route : String) {
+
+    object MainScreen : Screen("main_screen")
+
+    object DetailScreen : Screen("detail_screen")
+
+    fun withArgs(vararg args:String): String {
+        return buildString {
+            append(route)
+            args.forEach{arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
