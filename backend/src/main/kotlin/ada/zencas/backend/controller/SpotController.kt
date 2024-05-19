650d18bb90fa2a7f69ae21ce0fe223bd529d98ed
SamSam <samuel.lavazais@hotmail.fr>
2024-05-19 11:16:49 +0200

    controller : add CRUD endpoints

--- backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
+++ backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
@@ -2,11 +2,10 @@ package ada.zencas.backend.controller
 
 import ada.zencas.backend.data.model.SpotDto
 import ada.zencas.backend.service.SpotService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api")
@@ -15,9 +14,24 @@ class SpotController(private val spotService: SpotService) {
     @GetMapping("spots")
     fun getAllSpots(): ResponseEntity<List<SpotDto>> = ResponseEntity(spotService.getAllSpots(), HttpStatus.OK)
 
-    /* TODO: Compléter le controller en :
-    - indiquant les types de requête et les url
-    - créant les fonctions associées
-    - utilisant les fonctions du SpotService
-    */
+    // TODO
+    @GetMapping("task/{id}")
+    fun getTaskById(@PathVariable id: Long): ResponseEntity<SpotDto> =
+        ResponseEntity(spotService.getById(id), HttpStatus.OK)
+
+    @PostMapping("create")
+    fun createTask(
+        @Valid @RequestBody createRequest: SpotCreateRequest
+    ): ResponseEntity<SpotDto> = ResponseEntity(spotService.createSpot(createRequest), HttpStatus.OK)
+
+    @PatchMapping("update/{id}")
+    fun updateTask(
+        @PathVariable id: Long,
+        @Valid @RequestBody updateRequest: SpotUpdateRequest
+    ): ResponseEntity<SpotDto> = ResponseEntity(spotService.updateSpot(id, updateRequest), HttpStatus.OK)
+
+    @DeleteMapping("delete/{id}")
+    fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
+        ResponseEntity(spotService.deleteSpot(id), HttpStatus.OK)
+
 }
