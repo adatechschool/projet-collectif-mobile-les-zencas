232296b4ea496d2ed219229c93bf275db38bd26c
SamSam <samuel.lavazais@hotmail.fr>
2024-05-19 11:17:15 +0200

    Entity : fill Spot Entity

--- backend/src/main/kotlin/ada/zencas/backend/data/Spot.kt
+++ backend/src/main/kotlin/ada/zencas/backend/data/Spot.kt
@@ -4,7 +4,7 @@ import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
 
 @Entity
-@Table(name = "spot", uniqueConstraints = [UniqueConstraint(name = "uk_spot_name", columnNames = ["name"])])
+@Table(name = "spot")
 class Spot {
 
     @Id
@@ -16,5 +16,35 @@ class Spot {
     @Column(name = "name", nullable = false, unique = true)
     var name: String = ""
 
-    // TODO: compléter avec les autres colonnes/variables de cette table
+    @NotBlank
+    @Column(name = "category", nullable = false)
+    var category: String = ""
+
+    @Column(name = "description", nullable = false)
+    var description: String = ""
+
+    @NotBlank
+    @Column(name = "latitude", nullable = false)
+    var latitude: String = ""
+
+    @NotBlank
+    @Column(name = "longitude", nullable = false)
+    var longitude: String = ""
+
+    @NotBlank
+    @Column(name = "country", nullable = false)
+    var country: String = ""
+
+    @NotBlank
+    @Column(name = "city", nullable = false)
+    var city: String = ""
+
+    @NotBlank
+    @Column(name = "place", nullable = false)
+    var place: String = ""
+
+//    @NotBlank
+//    @Column(name = "user_id", nullable = false)
+//    var userId: String = ""
+
 }
