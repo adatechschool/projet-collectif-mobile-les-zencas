387d9ad1f300394eb1fd8d38c6a5e7eef819ee62
SamSam <samuel.lavazais@hotmail.fr>
2024-05-19 13:44:34 +0200

    fix endpoints names

--- backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
+++ backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
@@ -11,29 +11,28 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("spots")
 class SpotController(private val spotService: SpotService) {
 
-    @GetMapping("spots")
+    @GetMapping
     fun getAllSpots(): ResponseEntity<List<SpotBasic>> = ResponseEntity(spotService.getAllSpots(), HttpStatus.OK)
 
-    // TODO
-    @GetMapping("task/{id}")
+    @GetMapping("{id}")
     fun getTaskById(@PathVariable id: Long): ResponseEntity<SpotDetail> =
         ResponseEntity(spotService.getSpotById(id), HttpStatus.OK)
 
-    @PostMapping("create")
+    @PostMapping
     fun createTask(
         @Valid @RequestBody createRequest: SpotCreateRequest
     ): ResponseEntity<SpotDetail> = ResponseEntity(spotService.createSpot(createRequest), HttpStatus.OK)
 
-    @PatchMapping("update/{id}")
+    @PatchMapping("{id}")
     fun updateTask(
         @PathVariable id: Long,
         @Valid @RequestBody updateRequest: SpotUpdateRequest
     ): ResponseEntity<SpotDetail> = ResponseEntity(spotService.updateSpot(id, updateRequest), HttpStatus.OK)
 
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("{id}")
     fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
         ResponseEntity(spotService.deleteSpot(id), HttpStatus.OK)
 
