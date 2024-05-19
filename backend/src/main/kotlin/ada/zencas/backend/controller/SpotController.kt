81d20bc70d0611825c523d66c4436a0a47d3dc97
SamSam <samuel.lavazais@hotmail.fr>
2024-05-19 12:25:24 +0200

    update controller

--- backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
+++ backend/src/main/kotlin/ada/zencas/backend/controller/SpotController.kt
@@ -1,6 +1,9 @@
 package ada.zencas.backend.controller
 
-import ada.zencas.backend.data.model.SpotDto
+import ada.zencas.backend.data.model.SpotBasic
+import ada.zencas.backend.data.model.SpotCreateRequest
+import ada.zencas.backend.data.model.SpotDetail
+import ada.zencas.backend.data.model.SpotUpdateRequest
 import ada.zencas.backend.service.SpotService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
@@ -12,23 +15,23 @@ import org.springframework.web.bind.annotation.*
 class SpotController(private val spotService: SpotService) {
 
     @GetMapping("spots")
-    fun getAllSpots(): ResponseEntity<List<SpotDto>> = ResponseEntity(spotService.getAllSpots(), HttpStatus.OK)
+    fun getAllSpots(): ResponseEntity<List<SpotBasic>> = ResponseEntity(spotService.getAllSpots(), HttpStatus.OK)
 
     // TODO
     @GetMapping("task/{id}")
-    fun getTaskById(@PathVariable id: Long): ResponseEntity<SpotDto> =
-        ResponseEntity(spotService.getById(id), HttpStatus.OK)
+    fun getTaskById(@PathVariable id: Long): ResponseEntity<SpotDetail> =
+        ResponseEntity(spotService.getSpotById(id), HttpStatus.OK)
 
     @PostMapping("create")
     fun createTask(
         @Valid @RequestBody createRequest: SpotCreateRequest
-    ): ResponseEntity<SpotDto> = ResponseEntity(spotService.createSpot(createRequest), HttpStatus.OK)
+    ): ResponseEntity<SpotDetail> = ResponseEntity(spotService.createSpot(createRequest), HttpStatus.OK)
 
     @PatchMapping("update/{id}")
     fun updateTask(
         @PathVariable id: Long,
         @Valid @RequestBody updateRequest: SpotUpdateRequest
-    ): ResponseEntity<SpotDto> = ResponseEntity(spotService.updateSpot(id, updateRequest), HttpStatus.OK)
+    ): ResponseEntity<SpotDetail> = ResponseEntity(spotService.updateSpot(id, updateRequest), HttpStatus.OK)
 
     @DeleteMapping("delete/{id}")
     fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
