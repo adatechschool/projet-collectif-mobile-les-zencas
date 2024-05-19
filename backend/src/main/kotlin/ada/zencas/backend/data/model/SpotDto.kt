ff67786da6862d4b07f085f14ea1c2cdb63d63ba
SamSam <samuel.lavazais@hotmail.fr>
2024-05-19 11:29:19 +0200

    fill SpotDto

--- backend/src/main/kotlin/ada/zencas/backend/data/model/SpotDto.kt
+++ backend/src/main/kotlin/ada/zencas/backend/data/model/SpotDto.kt
@@ -1,9 +1,18 @@
 package ada.zencas.backend.data.model
 
+import org.springframework.context.annotation.Description
 import java.time.LocalDateTime
 
 // TODO: compléter le Dto en indiquant les caractéristiques de l'objt qui sera manipulé par ce back-end
 data class SpotDto(
     val id: Long,
     val name: String,
+    val category: String,
+    val description: String,
+    val latitude: String,
+    val longitude: String,
+    val country: String,
+    val city: String,
+    val place: String,
+    val createdOn: LocalDateTime
 )
